88d7f0a55823a847bb5e26e860ee9bf181380ed7
Jhan Diaz <jhandi77@gmail.com>
2022-01-24 21:48:38 -0300

    Case 14

---
+++ app.js
@@ -0,0 +1,11 @@
+const { buscarAutos } = require('./concesionario')
+const concesionarias = require('./concesionario')
+const compradores = JSON.parse(fs.readFileSync('./data/compradores.json', 'utf-8'))
+
+
+//console.log(concesionarias.autos)
+
+//console.log(concesionarias.buscarAutos("APL123"))
+//console.log(concesionarias.venderAuto("APL123"))
+//console.log(concesionarias.autosParaLaVenta ())
+console.log(1)
